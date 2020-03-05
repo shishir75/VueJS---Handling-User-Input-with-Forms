21765305a6853c6ef4e852ffc689388c0cde32cf
shishir75 <shishir.srdr16@gmail.com>
2020-03-06 00:35:14 +0600

    Design

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col-md-8 offset-md-2">
+        <h3>File a Complain</h3>
+        <form>
+          <div class="form-group">
+            <label>Email address</label>
+            <input type="email" class="form-control" placeholder="Enter email">
+            <small class="form-text text-muted"></small>
+          </div>
+
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" placeholder="Password">
+          </div>
+
+          <div class="form-group">
+            <label>Age</label>
+            <input type="number" class="form-control" placeholder="Enter Age">
+            <small class="form-text text-muted"></small>
+          </div>
+
+          <div class="form-group">
+            <label>Message</label>
+            <textarea class="form-control" cols="30" rows="5" placeholder="Enter Message"></textarea>
+          </div>
+
+          <div class="form-group form-inline">
+            <div class="custom-control custom-checkbox my-1 mr-sm-2">
+              <input type="checkbox" class="custom-control-input" id="customControlInline">
+              <label class="custom-control-label" for="customControlInline">Send Mail</label>
+            </div>
+            <div class="custom-control custom-checkbox my-1 mr-sm-2">
+              <input type="checkbox" class="custom-control-input" id="customControlInline1">
+              <label class="custom-control-label" for="customControlInline1">Send Infomail</label>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="custom-control custom-radio custom-control-inline">
+              <input type="radio" id="male" class="custom-control-input">
+              <label class="custom-control-label" for="male">Male</label>
+            </div>
+            <div class="custom-control custom-radio custom-control-inline">
+              <input type="radio" id="female" class="custom-control-input">
+              <label class="custom-control-label" for="female">Female</label>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="mr-sm-2">Priority</label>
+            <select class="custom-select mr-sm-2">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="row my-5">
+      <div class="col-md-8 offset-md-2">
+        <div class="card">
+          <div class="card-header">
+            Result
+          </div>
+          <div class="card-body">
+            <p class="card-text">Email: </p>
+            <p class="card-text">Password: </p>
+            <p class="card-text">Age: </p>
+            <p class="card-text">Message</p>
+            <p class="card-text">Sending Option: </p>
+            <p class="card-text">Gender: </p>
+            <p class="card-text">Priority: </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      
+    }
   }
+
 }
 </script>
+
 
 <style>
 #app {
